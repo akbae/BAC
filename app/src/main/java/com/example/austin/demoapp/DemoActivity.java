ecc9e5e6857ad3e31fd4466252907bcd3c234758
akbae <akb5@rice.edu>
2016-01-16 13:18:05 -0600

    Changed app name Added BAC calculation (unimplemented) Added second activity (settings) Added menu option Added FAB image Added snackbar text Attached FAB to drink count

--- app/src/main/java/com/example/austin/demoapp/DemoActivity.java
+++ app/src/main/java/com/example/austin/demoapp/DemoActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class DemoActivity extends AppCompatActivity {
+
+    public int drinksConsumed = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +23,32 @@ public class DemoActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        TextView drinkCount = (TextView) findViewById(R.id.DrinkCount);
+        drinkCount.setText("Drink Count: 0");
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "1 drink consumed", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show(); // listens for drink
+                drinksConsumed++;
+
+                TextView drinkCount = (TextView) findViewById(R.id.DrinkCount);
+                TextView BAC = (TextView) findViewById(R.id.BAC);
+
+                drinkCount.setText("Drink Count: " + String.valueOf(drinksConsumed));
+                BAC.setText("BAC: " + "");
             }
+
         });
+    }
+
+    public double BAC_calc(int drinks, String sex, int weight, double time)
+    {
+        double sexRatio = sex.equals("MALE") ? 0.73 : 0.66;
+        double BAC = (drinks * 5.14 / weight * sexRatio) - 0.015 * time;
+
+        return BAC;
     }
 
     @Override
@@ -43,8 +66,17 @@ public class DemoActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id)
+        {
+            case R.id.action_settings:
+                Intent intent = new Intent(this,Settings.class);
+                this.startActivity(intent);
+                return true;
+            case R.id.reset:
+                drinksConsumed = 0;
+                TextView drinkCount = (TextView) findViewById(R.id.DrinkCount);
+                drinkCount.setText("Drink Count: 0");
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/com/example/austin/demoapp/Settings.java
@@ -0,0 +1,21 @@
+package com.example.austin.demoapp;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Austin on 1/16/2016.
+ */
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+
+}
