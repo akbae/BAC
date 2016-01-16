af424d00f4b071ae4201ccb116d59f0890c74750
akbae <akb5@rice.edu>
2016-01-16 15:07:34 -0600

    Changed settings -> new activity to settings -> full screen dialog Changed listener for updateBAC to be embedded in xml and added its method in DemoActivity

--- app/src/main/java/com/example/austin/demoapp/DemoActivity.java
+++ app/src/main/java/com/example/austin/demoapp/DemoActivity.java
@@ -8,8 +8,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 import android.widget.TextView;
+import android.widget.Button;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 public class DemoActivity extends AppCompatActivity {
 
@@ -38,9 +43,43 @@ public class DemoActivity extends AppCompatActivity {
 
                 drinkCount.setText("Drink Count: " + String.valueOf(drinksConsumed));
                 BAC.setText("BAC: " + "");
-            }
+            }//onClick
 
-        });
+        });//fab.setOnClickListener
+
+    }//onCreate
+
+    static public class settingsDialog extends DialogFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setStyle(DialogFragment.STYLE_NORMAL, R.style.settings_dialog);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            Dialog d = getDialog();
+            if (d!=null){
+                int width = ViewGroup.LayoutParams.MATCH_PARENT;
+                int height = ViewGroup.LayoutParams.MATCH_PARENT;
+                d.getWindow().setLayout(width, height);
+            }//if
+        }//onStart
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View root = inflater.inflate(R.layout.settings, container, false);
+            return root;
+        }
+
+    }//settingsDialog
+
+    public void updateBAC(View view)
+    {
+        TextView BAC = (TextView) findViewById(R.id.BAC);
+        BAC.setText("");
     }
 
     public double BAC_calc(int drinks, String sex, int weight, double time)
@@ -49,14 +88,14 @@ public class DemoActivity extends AppCompatActivity {
         double BAC = (drinks * 5.14 / weight * sexRatio) - 0.015 * time;
 
         return BAC;
-    }
+    }//BAC_calc
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_demo, menu);
         return true;
-    }
+    }//onCreateOptionsMenu
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -69,16 +108,18 @@ public class DemoActivity extends AppCompatActivity {
         switch (id)
         {
             case R.id.action_settings:
-                Intent intent = new Intent(this,Settings.class);
-                this.startActivity(intent);
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                settingsDialog sD_fragment = new settingsDialog();
+                sD_fragment.show(ft, "txn_tag");
                 return true;
             case R.id.reset:
                 drinksConsumed = 0;
                 TextView drinkCount = (TextView) findViewById(R.id.DrinkCount);
                 drinkCount.setText("Drink Count: 0");
                 return true;
-        }
+        }//switch
 
         return super.onOptionsItemSelected(item);
-    }
-}
+    }//onOptionsItemsSelected
+
+}//DemoActivity
